87eeb8ed2d408601d72b4371b4f708d89f3f8d61
manoj-hubgit <manojannadurai2265@gmail.com>
2024-07-06 09:52:58 +0530

    files added

--- Controllers/urlController.js
+++ Controllers/urlController.js
@@ -1,83 +1,43 @@
 import shortid from "shortid";
 import Url from "../Models/urlSchema.js";
 
-export const createShortUrl = async (req, res) => {
-  const { longUrl } = req.body;
+export const createShortUrl = async(req,res) =>{
+    const {longUrl} =req.body;
 
-  try {
-    let url = await Url.findOne({ longUrl });
-    if (url) {
-      return res.json(url);
+try {
+   
+    let url = await Url.findOne({longUrl});
+    if(url){
+        return res.json(url);
     }
-    const urlCode = shortid.generate();
-    const shortUrl = `https://shortner-backend-c4dw.onrender.com/api/url/${urlCode}`;
+  const urlCode=shortid.generate();
+    const shortUrl=`https://shortner-backend-c4dw.onrender.com/api/url/${urlCode}`;
     url = new Url({
-      longUrl,
-      shortUrl,
-      urlCode,
-      date: new Date(),
-    });
-    await url.save();
-    res.json(url);
-  } catch (error) {
+        longUrl,
+        shortUrl,
+        urlCode,
+        date: new Date(),
+      }); 
+      await url.save();
+      res.json(url);
+} catch (error) {
     console.error(error);
-    res.status(500).json("Internal Server error");
-  }
-};
+    res.status(500).json('Internal Server error');
+}
+}
 
-export const redirectUrl = async (req, res) => {
-  try {
-    const url = await Url.findOne({ urlCode: req.params.code });
-    if (url) {
-      return res.redirect(url.longUrl);
-    } else {
-      return res.status(404).json("No URL found");
+export const redirectUrl = async(req,res)=>{
+    try {
+        const url = await Url.findOne({urlCode:req.params.code})
+        if(url){
+            return res.redirect(url.longUrl);
+        }
+        else{
+            return res.status(404).json('No URL found');
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json('Internal Server error');
     }
-  } catch (error) {
-    console.error(error);
-    res.status(500).json("Internal Server error");
-  }
-};
+}
 
-export const getUrlStats = async (req, res) => {
-  try {
-    const dailyStats = await Url.aggregate([
-      {
-        $match: {
-          date: {
-            $gte: new Date(new Date().setDate(new Date().getDate() - 30)),
-          },
-        },
-      },
-      {
-        $group: {
-          _id: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
-          count: { $sum: 1 },
-        },
-      },
-      { $sort: { _id: 1 } },
-    ]);
-
-    const monthlyStats = await Url.aggregate([
-      {
-        $match: {
-          date: {
-            $gte: new Date(new Date().setMonth(new Date().getMonth() - 12)),
-          },
-        },
-      },
-      {
-        $group: {
-          _id: { $dateToString: { format: "%Y-%m", date: "$date" } },
-          count: { $sum: 1 },
-        },
-      },
-      { $sort: { _id: 1 } },
-    ]);
-
-    res.json({ dailyStats, monthlyStats });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json("Internal Server error");
-  }
-};
--- Routers/urlRoute.js
+++ Routers/urlRoute.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { createShortUrl, getUrlStats, redirectUrl } from "../Controllers/urlController.js";
+import { createShortUrl, redirectUrl } from "../Controllers/urlController.js";
 
 const router = express.Router();
 
 router.post('/shorten',createShortUrl)
 router.get('/:code',redirectUrl);
-router.get('/stats',getUrlStats)
+
 export default router;
